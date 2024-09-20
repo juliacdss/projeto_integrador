8822cc185e6577bc5bfd970f1c1404fad6ad92c9
annaaraujoifc <annaluizaaraujo.ifcaraquari@gmail.com>
2024-09-20 18:32:41 -0300

    mudanças ganharpeso

--- js/main.js
+++ js/main.js
@@ -1,20 +1,8 @@
+function showInfo(dishId) {
+  document.getElementById(dishId).style.display = 'block';
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-  const ingredientesContainer = document.getElementById('armazenar-alimentos');
+function hideInfo(dishId) {
+  document.getElementById(dishId).style.display = 'none';
+}
 
-  // Adiciona um evento de clique para cada ingrediente
-  document.querySelectorAll('.ingredient').forEach(ingredient => {
-    ingredient.addEventListener('click', (event) => {
-      const img = document.createElement('img');
-      img.src = event.target.src;
-      img.alt = event.target.alt;
-      img.className = 'alimentos-no-prato';
-      img.style.position = 'absolute';
-      img.style.top = `${Math.random() * 90}%`; // Posição aleatória dentro do prato
-      img.style.left = `${Math.random() * 90}%`; // Posição aleatória dentro do prato
-      img.style.transform = 'translate(-50%, -50%)'; // Ajusta o centro da imagem
-
-      ingredientesContainer.appendChild(img);
-    });
-  });
-});
